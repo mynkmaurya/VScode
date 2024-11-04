2dc84f0800d4c6792db628fa9e68fb954abc90e1
Mayank Maurya <mayankmaurya3435@gmail.com>
2024-11-04 13:56:01 +0530

    Mayank

---
+++ CPP/loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int r, c, i;
+
+
+for (i = 1; i <= 5; i++) {
+    for (c = 5-(i-1); c >= 1 ; c--) {
+        cout <<c << " " ;
+    }
+   cout << endl; 
+}
+
+    return 0;
+}
